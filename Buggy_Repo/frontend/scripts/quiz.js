bcdeba04c6aea2f0d78ee2a412c079d5abba2357
Tanush-IIITH <tanushgarg18@gmail.com>
2025-04-16 15:08:58 +0530

    Change css and quiz

--- Buggy_Repo/frontend/scripts/quiz.js
+++ Buggy_Repo/frontend/scripts/quiz.js
@@ -36,7 +36,7 @@ function updateAttempts() {
 }
 
 searchInput.addEventListener("input", updateAttempts);
-// how is life ?
+
 async function loadHighScore() {
   try {
     const res = await fetch(`${BASE_URL}/quiz/highscore`);
@@ -116,7 +116,8 @@ form.addEventListener("submit", async (e) => {
       form.innerHTML = "";
       resetBtn.classList.remove("hidden");
     }
-  } catch {
+  } catch (error) {
+    console.error("Error submitting answer:", error);
     feedback.textContent = "Error submitting answer.";
   }
 });
