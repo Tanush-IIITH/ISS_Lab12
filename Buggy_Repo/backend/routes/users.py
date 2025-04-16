7ebada8bcc6f79062f1888f6592f87df1bd6b814
RISHABH914 <rishabhgoyal.champion123@gmail.com>
2025-04-16 15:57:49 +0530

    changed users.py, changed post tot get and delete all to delete one

--- Buggy_Repo/backend/routes/users.py
+++ Buggy_Repo/backend/routes/users.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter, HTTPException
 from models import User
 from bson import ObjectId
-
+from bson.errors import InvalidId
 router = APIRouter()
 
 async def get_users_collection():
     from db import init_db
     return init_db()["users_collection"]
 
-@router.post("/")
+#changed post to get
+@router.get("/")
 async def get_users():
     collection = await get_users_collection()
     users = []
@@ -26,8 +27,14 @@ async def create_user(user: User):
 
 @router.delete("/{user_id}")
 async def delete_user(user_id: str):
+    try:
+        obj_id=ObjectId(user_id)
+    except InvalidId:
+        raise HTTPException(status_code=400, detail="Invalid user ID format")
+
     collection = await get_users_collection()
-    result = await collection.delete_all()
+    #fixed: changed delete all to delete one with a filter
+    result = await collection.delete_one({"_id": ObjectId(user_id)})
     if result.deleted_count:
         return {"status": "deleted"}
     raise HTTPException(status_code=404, detail="User not found")
